f65db1d6d96c8addf125601e664a2a30d4d83e01
zhangruihua <zhangruihua@mininglamp.com>
2020-06-03 20:52:07 +0800

    first commit

---
+++ src/main/java/com/zrh/javagradledemo/JavaGradleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zrh.javagradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGradleDemoApplication.class, args);
+	}
+
+}

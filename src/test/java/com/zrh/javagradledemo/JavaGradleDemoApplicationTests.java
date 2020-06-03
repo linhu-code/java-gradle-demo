032c40adbcaa90632f0b58dba9390759452a7116
zhangruihua <zhangruihua@mininglamp.com>
2020-06-03 21:12:06 +0800

    新增yml配置文件

--- src/test/java/com/zrh/javagradledemo/JavaGradleDemoApplicationTests.java
+++ src/test/java/com/zrh/javagradledemo/JavaGradleDemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.zrh.javagradledemo;
 
+import com.zrh.javagradledemo.Users.Users;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaGradleDemoApplicationTests {
+    @Autowired
+    public Users users;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        System.out.printf("这个名子是：%s", users.getName());
+
+    }
 
 }
